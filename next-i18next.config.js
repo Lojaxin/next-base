812ac2e157095d94aff08f95512e20ad0f81479a
jason.lawjx <jason.lawjx@finpoints.com>
2025-03-21 14:49:03 +0800

    feat: 删除多语言配置next-i18next和localePath的冲突

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next-i18next').UserConfig} */
 
-const path = require('path');
-
 module.exports = {
     i18n: {
         defaultLocale: 'cn',
         locales: ['en', 'cn'],
-        localePath: path.resolve('./public/locales'),
     },
 };
